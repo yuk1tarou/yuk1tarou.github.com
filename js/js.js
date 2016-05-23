9b70e2e12f59524f09d03107920cc319cf576c8f
ydb <ydb@89mc.com>
2016-05-23 14:11:15 +0800

    asdasda

--- js/js.js
+++ js/js.js
@@ -7,21 +7,23 @@ window.onresize=function(){
 	document.documentElement.style.fontSize = cW*(100/320)+ 'px';
 };
 var oUl=document.getElementById("ul1");
-function move(ev){
-	var l=(ev.targetTouches[0].pageX-disX)/2/100;
-		console.log(l)
-	if(l>0){
-		l=0;
-	}
-	oUl.style.left=l+'rem';
-	document.addEventListener('touchend',end,false)
-}
-function end(){
-	document.removeEventListener('touchmove',move,false);
-	document.removeEventListener('touchend',end,false)
-}
 oUl.addEventListener('touchstart',function(ev){	
-		disX=ev.targetTouches[0].pageX-oUl.offsetLeft;
+		disX=ev.targetTouches[0].pageX-oUl.offsetLeft/2/100;
 		document.addEventListener('touchmove',move,false)
 		ev.preventDefault();
+	function move(ev){
+		var l=(ev.targetTouches[0].pageX-disX)/2/100;
+			console.log(l)
+		if(l>0){
+			l=0;
+		}else if(l<-2.2){
+			l=-2.2
+		}
+		oUl.style.left=l+'rem';
+		document.addEventListener('touchend',end,false)
+	}
+	function end(){
+		document.removeEventListener('touchmove',move,false);
+		document.removeEventListener('touchend',end,false)
+	}
 },false);
