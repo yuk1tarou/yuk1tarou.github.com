6fd87655d84ea341b7f5635a93efe2d92aa38141
ydb <ydb@89mc.com>
2016-05-23 14:36:43 +0800

    asdasda

--- js/js.js
+++ js/js.js
@@ -7,12 +7,13 @@ window.onresize=function(){
 	document.documentElement.style.fontSize = cW*(100/320)+ 'px';
 };
 var oUl=document.getElementById("ul1");
+var l=0;
 oUl.addEventListener('touchstart',function(ev){	
 		disX=ev.targetTouches[0].pageX-oUl.offsetLeft/2/100;
 		document.addEventListener('touchmove',move,false)
 		ev.preventDefault();
 	function move(ev){
-		var l=(ev.targetTouches[0].pageX-disX)/2/100;
+		l=(ev.targetTouches[0].pageX-disX)/2/100;
 			console.log(l)
 		if(l>0){
 			l=0;
