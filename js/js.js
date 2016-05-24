24307221a67e0196ae7caec9a395575106f54c41
ydb <ydb@89mc.com>
2016-05-24 11:19:10 +0800

    this is

--- js/js.js
+++ js/js.js
@@ -1,5 +1,12 @@
+var cW = document.documentElement.clientWidth;
+if(cW > 640) cW = 640;
+document.documentElement.style.fontSize = cW*(100/320) + 'px';
 window.onresize=function(){
-	document.documentElement.style.fontSize = document.documentElement.clientWidth*(100/320)+'px';
+	var cW = document.documentElement.clientWidth;
+	if(cW > 640) cW = 640;
+	document.documentElement.style.fontSize = cW*(100/320)+ 'px';
+	
+};
 	var oUl=document.getElementById("ul1");
 	var l=0;
 	var rem=parseFloat(document.documentElement.style.fontSize);
@@ -7,12 +14,13 @@ window.onresize=function(){
 		var disX=ev.targetTouches[0].pageX/rem-l;
 		document.addEventListener('touchmove',move,false);
 		ev.preventDefault();
+		function move(ev){
 		if(l>0){
 			l=0;
 		}else if(l<-2.2){
 			l=-2.2;
 		}
-		function move(ev){
+			console.log(l)
 			l=ev.targetTouches[0].pageX/rem-disX;
 			oUl.style.webkitTransform='translate('+l+'rem,0)';
 		}
@@ -22,5 +30,3 @@ window.onresize=function(){
 		}
 			document.addEventListener('touchend',end,false);
 	},false);
-	
-};
